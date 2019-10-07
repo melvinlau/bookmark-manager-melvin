ad8b022ebdcf284bdc5a6bd6551809c2b1e11a91
Dom Tunstill <datunstill@gmail.com>
2019-10-07 17:11:43 +0100

    Test commit

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -3,7 +3,7 @@ require 'pg'
 class Bookmarks
   def self.all
     db = PG.connect(dbname: 'bookmark_manager')
-    p result = db.exec("SELECT * FROM bookmarks;")
+    result = db.exec("SELECT * FROM bookmarks;")
     result.map { |bookmark| bookmark['url'] }
   end
 end
