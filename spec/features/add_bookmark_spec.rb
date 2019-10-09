d0ffd9d85162a4f186a5504ba9df79be4d87800a
Melvin Lau <lau.melvin@icloud.com>
2019-10-09 22:52:02 +0100

    Added URL validation test

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -1,4 +1,5 @@
 feature "Add a bookmark" do
+
   scenario "User can add a site URL" do
     visit "/bookmarks/new"
     fill_in "url", with: "https://www.makers.tech"
@@ -6,4 +7,13 @@ feature "Add a bookmark" do
     click_button "Submit"
     expect(page).to have_link("Makers", href: "https://www.makers.tech")
   end
+
+  scenario "Bookmark must be a valid URL" do
+    visit "/bookmarks/new"
+    fill_in "url", with: "not a real bookmark"
+    click_button "Submit"
+    expect(page).not_to have_content "not a real bookmark"
+    expect(page).to have_content "You must submit a valid URL."
+  end
+
 end
